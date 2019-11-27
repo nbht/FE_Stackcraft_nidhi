447606fc7d187ff52828aac282f7fb6a43cf9b82
NIDHI BHATNAGAR <nidhi.bhatnagar@orange.com>
2019-11-27 06:41:33 +0000

    Update Service Catalog Terraform: 111-cce-fe

--- 111-cce-fe/terraform/module-111-cce-fe/config.tf
+++ 111-cce-fe/terraform/module-111-cce-fe/config.tf
@@ -18,6 +18,20 @@
   "token" = "test"
 }
 
+"resource" "flexibleengine_cce_cluster_v3" "nidhi_cce" {
+  "cluster_type" = "VirtualMachine"
+
+  "container_network_type" = "overlay_l2"
+
+  "flavor_id" = "cce.s2.small"
+
+  "name" = "nidhi_cce_stack"
+
+  "subnet_id" = "ef4f624b-bd3f-4e93-a53a-3ca3e08d8e21"
+
+  "vpc_id" = "1fd7a904-2367-4a43-ae49-1351588387d6"
+}
+
 "variable" "env" {
   "default" = "staging"
 
